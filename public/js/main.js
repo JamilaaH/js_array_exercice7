59cf4d05db74b6b2dd47034da4396d9db2036e70
Jamila <jaja_mimi@hotmail.com>
2021-01-04 16:16:14 +0100

    fin

---
+++ public/js/main.js
@@ -0,0 +1,58 @@
+//exo1
+let prenoms = ["Ayhan", "Jamila", "Ayoub", "Louise"];
+console.log(prenoms);
+
+console.log(prenoms[3]);
+console.log(prenoms.length);
+
+// exo2
+let tab = ["Jamila"];
+console.log(tab);
+tab.push("Louise");
+console.log(tab);
+
+// exo3
+let coding16 = ["Ayhan"];
+
+coding16.push("Benedicte");
+console.log(`${coding16[1]} arrive à 8h44`);
+
+coding16.push("Louise", "Jamila");
+console.log(`${coding16[2]} et ${coding16[3]} rejoignent la classe`);
+
+coding16.push("Sebastien", "Adame");
+console.log(`${coding16[4]} et ${coding16[5]} rejoignent la classe`);
+
+coding16.push("Dawid", "Malo");
+console.log(`${coding16[6]} et ${coding16[7]} rentrent en classe`);
+
+coding16.push("Alex");
+console.log(`${coding16[8]} rentre la classe`);
+
+coding16.pop();
+console.log("puis il redescend");
+// console.log(coding16);
+
+coding16.push("Maxence", "Mohamed");
+console.log(`${coding16[8]} et ${coding16[9]} rejoignent la classe`);
+
+coding16.push("Saliou");
+console.log(`${coding16[10]} rentre en classe tranquille`);
+
+coding16.push("Zulma", "Max");
+console.log(`${coding16[11]} et ${coding16[12]} rejoignent la classe`);
+
+coding16.push("Natchez");
+console.log(`${coding16[13]} rentre en classe tranquille`);
+
+coding16.push("Zakaria", "Ayoub");
+console.log(`${coding16[14]} et ${coding16[15]} rejoignent la classe`);
+
+coding16.push("Achraf");
+console.log(`${coding16[15]} rentre en classe tranquille`);
+
+coding16.push("Abdellah");
+console.log(`${coding16[16]} rentre en classe avec un min de bruit`);
+
+
+// exo4
